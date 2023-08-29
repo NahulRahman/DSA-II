fa5cd824add2bd2d26c3ce844300f704fdd795ca
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-08-30 01:27:02 +0600

    Add files via upload

--- 28_08_heap_class.cpp
+++ 28_08_heap_class.cpp
@@ -3,127 +3,160 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Heap{
+class Heap {
 private:
     int a[101], size;
 
 public:
-    Heap(){
+    Heap() {
         size = 0;
     }
 
 private:
-    void bottomTopAdjust(int i){
-
-
+    void bottomTopAdjust(int i) {
+        while (i > 1 && a[i] > a[i / 2]) {
+            swap(a[i], a[i / 2]);
+            i = i / 2;
+        }
     }
 
-    void topBottomAdjust(int i){
-
-
+    void topBottomAdjust(int i) {
+        int child;
+        while (2 * i <= size) {
+            child = 2 * i;
+            if (child + 1 <= size && a[child + 1] > a[child])
+                child++;
+            if (a[i] >= a[child])
+                break;
+            swap(a[i], a[child]);
+            i = child;
+        }
     }
 
 public:
-    bool insert(int val){
-
+    bool insert(int val) {
+        if (size >= 100)
+            return false;
+        size++;
+        a[size] = val;
+        bottomTopAdjust(size);
+        return true;
     }
 
-    int showMax(){
-
+    int showMax() {
+        if (size == 0)
+            return -1; // Assuming -1 is an invalid value
+        return a[1];
     }
 
-    int showSize(){
-
+    int showSize() {
+        return size;
     }
 
-    bool deleteRoot(){
-
-
+    bool deleteRoot() {
+        if (size == 0)
+            return false;
+        swap(a[1], a[size]);
+        size--;
+        topBottomAdjust(1);
+        return true;
     }
 
-    void buildHeap(){
-
+    void buildHeap() {
+        for (int i = size / 2; i >= 1; i--) {
+            topBottomAdjust(i);
+        }
     }
 
-    void sort(){
-
+    void sort() {
+        int heapSize = size;
+        while (size > 1) {
+            swap(a[1], a[size]);
+            size--;
+            topBottomAdjust(1);
+        }
+        size = heapSize; // Restore the original size
     }
 
-    void bfs(){
-        if(size==0) return;
-        int level = 2;
-        queue<int>q;
+    void bfs() {
+        if (size == 0)
+            return;
+        int level = 1;
+        queue<int> q;
         q.push(1);
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int parent = q.front();
             q.pop();
-            if(parent==level){
-                cout<<endl;
+            if (parent == level) {
+                cout << endl;
                 level = level * 2;
             }
-            cout<<a[parent]<<" ";
-            if(2*parent <= size) q.push(2*parent);
-            if(2*parent + 1 <= size) q.push(2*parent + 1);
+            cout << a[parent] << " ";
+            if (2 * parent <= size) q.push(2 * parent);
+            if (2 * parent + 1 <= size) q.push(2 * parent + 1);
         }
     }
 };
 
-int main(){
+int main() {
 
     Heap heap;
 
-    while(1){
-        cout<<"1. Insert    2. Show Max    3. Delete Max    4. Sort    5. Level    6. Build Heap    7. End"<<endl<<endl;
+    while (true) {
+        cout << "1. Insert    2. Show Max    3. Delete Max    4. Sort    5. Level    6. Build Heap    7. End" << endl << endl;
         int choice;
-        cin>>choice;
+        cin >> choice;
 
-        if(choice==1){
-            cout<<"Insert Value: ";
+        if (choice == 1) {
+            cout << "Insert Value: ";
             int y;
-            cin>>y;
+            cin >> y;
             bool b = heap.insert(y);
 
-            if(b)   cout<<y<<" is inserted in the heap"<<endl;
+            if (b)   cout << y << " is inserted in the heap" << endl;
         }
 
-        else if(choice==2){
-            if(heap.showSize()!=0)  cout<<"Max Element: "<<heap.showMax();
-            else    cout<<"No element in the heap"<<endl;
+        else if (choice == 2) {
+            if (heap.showSize() != 0)  cout << "Max Element: " << heap.showMax();
+            else    cout << "No element in the heap" << endl;
         }
 
-        else if(choice==3){
+        else if (choice == 3) {
             bool b = heap.deleteRoot();
-            if(b)   cout<<"Root deleted from heap";
-            else    cout<<"Heap is empty";
-            cout<<endl;
+            if (b)   cout << "Root deleted from heap";
+            else    cout << "Heap is empty";
+            cout << endl;
         }
 
-        else if(choice==4){
+        else if (choice == 4) {
             heap.sort();
         }
 
-        else if(choice==5){
-            cout<<"Level Wise Traversal of the heap:"<<endl;
+        else if (choice == 5) {
+            cout << "Level Wise Traversal of the heap:" << endl;
             heap.bfs();
-            cout<<endl;
+            cout << endl;
         }
-        else if(choice==6){
-            if(heap.showSize()==0)
-              cout<<"Heap is Empty!"<<endl;
+        else if (choice == 6) {
+            if (heap.showSize() == 0)
+                cout << "Heap is Empty!" << endl;
             else
-              heap.buildHeap();
+                heap.buildHeap();
         }
-        else if(choice==7){
+        else if (choice == 7) {
             break;
         }
 
-        else{
-            cout<<"Invalid Choice"<<endl;
+        else {
+            cout << "Invalid Choice" << endl;
         }
-        cout<<endl;
+        cout << endl;
     }
+
+    return 0;
 }
+
 
 /*
 1 2
